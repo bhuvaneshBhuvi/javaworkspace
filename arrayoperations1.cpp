21d5ef336d9cb9bf28282805c62f4a081014fa9f
Infant Ajay <infantcsajay@outlook.com>
2018-02-09 06:30:26 +0530

    Too many Changes Required
    I've commented the code with an opinion read it well. ping me if any doubts?

--- arrayoperations1.cpp
+++ arrayoperations1.cpp
@@ -10,7 +10,7 @@ int main()
 	int a[100],n,i,val,pos,ch;
 	printf("enter the number of elements in the array: ");
 	scanf("%d",&n);
-	printf("enter the elements of the array: ");
+	printf("enter the elements of the array: ");				//Then What is the purpose of insert function?
 	for(i=0;i<n;i++)
 	{
 		printf("a[%d]: ", i+1);
@@ -27,9 +27,9 @@ int main()
 	printf("\n6. delete at end.");
 	printf("enter your choice: ");
 	scanf("%d", &ch);
-   }while(ch<=6 && ch!=0);
+   }while(ch<=6 && ch!=0);							//when will the control reach switch statement?
 	switch(ch)
-	{
+	{									//why are the cases not in order?
 		if(ch == 1)
 		{
 			printf("\nenter the value to be inserted: ");
@@ -73,8 +73,8 @@ int main()
 		}
 	}
 }
-void append_begin(int arr[], int pos, int val, int n)
-{
+void append_begin(int arr[], int pos, int val, int n)				//If its append_begin why get pos?
+{										//What is the purpose val?
 	int i;
 	for(i=n;i>0;i--)
 	{
@@ -82,28 +82,28 @@ void append_begin(int arr[], int pos, int val, int n)
 	}
 	arr[0] = pos;
 }
-void insert_pos(int arr[], int pos, int val, int n)
+void insert_pos(int arr[], int pos, int val, int n)				//refactor to insert_at() or simply insert()
 {
 	int i;
 	for(i=n;i>=pos;i--)
-	  arr[i+1]=arr[i];
-	arr[pos] = val;
-	n+=1;
+	  arr[i+1]=arr[i];							//if n is size of array , then your iteration starts by
+	arr[pos] = val;								// assigning n+1. is it right assinging a 11th value to 
+	n+=1;									// an array of size 10?
 	
 }
 void append_end(int arr[], int val, int n)
 {
-	int i;
+	int i;									//what if n is already the maximum capacity of the array?
 	n=n+1;
 	arr[n] = val;
 }
 void del_start(int arr[], int n)
 {
-	n = n + 1;
+	n = n + 1;								//How will this work?
 }
 void del_end(int arr[], int n)
 {
-	n = n - 1;
+	n = n - 1;								//aren't you supposed to return the value of n?
 }
 void del_pos(int arr[], int pos, int n)
 {
@@ -118,3 +118,15 @@ void display(int arr[], int n)
 	for(i=0;i<n;i++)
 	  printf(" %d ", arr[i]);
 }
+
+
+/**************************************************************************************/
+/* 1. You have not checked array boundary any where!!!. What if I want to insert an   */
+/*	element in an already full array? What if I want to delete from an already    */
+/*	empty array?								      */
+/* 2. You have never updated the values !!!.When you modify the array its fine , its a*/
+/*	pointer, but what happens to the original 'n'? why does every operation return*/
+/*	void?									      */
+/* 3. Are you familiar at all with the concept of loops? What mess have you done with */
+/*	the menu driver? Did you execute the program at least once? Did it work?      */
+/**************************************************************************************/
